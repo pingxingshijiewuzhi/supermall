e1348285c46417799fbe45c8e51136f4e9a79738
pingxingshijiewuzhi <pingxingshijiewuzhi@example.com>
2020-02-27 22:23:08 +0800

    last

--- src/network/request.js
+++ src/network/request.js
@@ -5,8 +5,8 @@ export function request(config) {
     const instance = axios.create({
         // baseURL:"http://106.54.54.237:8000",
         // baseURL: 'http://123.207.32.32:8000/api/hy',
-        // 备用接口
-        baseURL:"http://106.54.54.237:8000/api/hy",
+        // 失效接口
+        // baseURL:"http://106.54.54.237:8000/api/hy",
         timeout: 1000
     })
         // 拦截请求信息
