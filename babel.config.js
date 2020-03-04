37ac48fef4297fd9fa9d90eb058d2547a51b07c1
pingxingshijiewuzhi <pingxingshijiewuzhi@example.com>
2020-03-04 20:57:45 +0800

    20200304

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,26 @@
+// 项目发布阶段用到插件
+// const prodPlugins = []
+// if (process.env.NODE_ENV === 'production') {
+//   prodPlugins.push('transform-remove-console')
+// }
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+
+  // plugins: [
+  
+  //   "transform-remove-console",
+// 发布产品时的数组,取代上面一行代码
+// ...prodPlugins
+
+  // [
+  //   'component',
+  // {
+  //   libraryName: 'element-ui',
+  //   styleLibraryName:'theme-chalk'
+  //   }
+  // ],
+  // ]
 }
--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,16 @@ module.exports = {
                 "views":"@/views"
             }
         }
-    }
+    },
+    // 为开发模式与发布模式指定不同的打包入口
+    // 开发模式入口文件：scr/ main - dev.js
+    // 发布模式入口文件:src/main-prod.js
+    // chainWebpack: config => { 
+    //     config.when(process.env.NODE_ENV === 'production', config => {
+    //         config.entry('app').clear().add('./src/main-prod.js')
+    //     })
+    //     config.when(process.env.NODE_ENV === 'development', config => {
+    //         config.entry('app').clear().add('./src/main-dev.js')
+    //     })
+    // }
 }
